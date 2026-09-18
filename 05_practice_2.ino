49f7221724b8b35e39c72238adc4cc832a8154b5
nnin23 <ndnnjwgzh4@privaterelay.appleid.com>
2026-09-18 12:57:48 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,28 @@
+#define LED_PIN 7
+
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+}
+
+void loop()
+{
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);
+
+    for (int i = 0; i < 5; i++)
+    {
+        digitalWrite(LED_PIN, HIGH);
+        delay(100);
+
+        digitalWrite(LED_PIN, LOW);
+        delay(100);
+    }
+
+    digitalWrite(LED_PIN, LOW);
+
+    while (1)
+    {
+    }
+}
